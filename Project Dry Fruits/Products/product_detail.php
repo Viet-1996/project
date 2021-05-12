49e74e4b37a7b1ab3897d687f107d4f0454be946
Viet-1996 <75302099+Viet-1996@users.noreply.github.com>
2021-05-12 16:09:11 +0700

    Update product_detail.php

--- Project Dry Fruits/Products/product_detail.php
+++ Project Dry Fruits/Products/product_detail.php
@@ -10,7 +10,6 @@
 <h1>Chi tiết sản phẩm</h1>
 <section>
 	<section><img class="img-fluid" src="img/<?=$result['image']?>"></section>
-	<section>...</section>
 	<section><?=$result['description']?></section>
  	<h2>Bình luận cho sản phẩm</h2>
 	<?php if(mysqli_num_rows($comments)==0):?>
